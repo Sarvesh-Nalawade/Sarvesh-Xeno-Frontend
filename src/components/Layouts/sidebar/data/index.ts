be8b6e39a68d858c910ead592f7e4979f8022e8e
Sarvesh-Nalawade <nalawadesarvesh98@gmail.com>
2025-09-14 03:42:24 +0530

    edit  sidebar

--- src/components/Layouts/sidebar/data/index.ts
+++ src/components/Layouts/sidebar/data/index.ts
@@ -5,106 +5,34 @@ export const NAV_DATA = [
     label: "MAIN MENU",
     items: [
       {
-        title: "Dashboard",
+        title: "Orders",
+        url: "/",
         icon: Icons.HomeIcon,
-        items: [
-          {
-            title: "Orders",
-            url: "/",
-          },
-          {
-            title: "Customers",
-            url: "/customers",
-          },
-          {
-            title: "Products",
-            url: "/products",
-          },
-          
-        ],
+        items: [],
       },
-      // {
-      //   title: "Calendar",
-      //   url: "/calendar",
-      //   icon: Icons.Calendar,
-      //   items: [],
-      // },
+      {
+        title: "Customers",
+        url: "/customers",
+        icon: Icons.HomeIcon,
+        items: [],
+      },
+      {
+        title: "Products",
+        url: "/products",
+        icon: Icons.HomeIcon,
+        items: [],
+      },
       {
         title: "Profile",
         url: "/profile",
         icon: Icons.User,
         items: [],
       },
-      {
-        title: "Shop Details",
-        url: "/shop",
-        icon: Icons.User,
-        items: [],
-      },
-      // {
-      //   title: "Forms",
-      //   icon: Icons.Alphabet,
-      //   items: [
-      //     {
-      //       title: "Form Elements",
-      //       url: "/forms/form-elements",
-      //     },
-      //     {
-      //       title: "Form Layout",
-      //       url: "/forms/form-layout",
-      //     },
-      //   ],
-      // },
-      // {
-      //   title: "Tables",
-      //   url: "/tables",
-      //   icon: Icons.Table,
-      //   items: [
-      //     {
-      //       title: "Tables",
-      //       url: "/tables",
-      //     },
-      //   ],
-      // },
-      // {
-      //   title: "Pages",
-      //   icon: Icons.Alphabet,
-      //   items: [
-      //     {
-      //       title: "Settings",
-      //       url: "/pages/settings",
-      //     },
-      //   ],
-      // },
     ],
   },
   {
     label: "OTHERS",
     items: [
-      // {
-      //   title: "Charts",
-      //   icon: Icons.PieChart,
-      //   items: [
-      //     {
-      //       title: "Basic Chart",
-      //       url: "/charts/basic-chart",
-      //     },
-      //   ],
-      // },
-      // {
-      //   title: "UI Elements",
-      //   icon: Icons.FourCircle,
-      //   items: [
-      //     {
-      //       title: "Alerts",
-      //       url: "/ui-elements/alerts",
-      //     },
-      //     // {
-      //     //   title: "Buttons",
-      //     //   url: "/ui-elements/buttons",
-      //     // },
-      //   ],
-      // },
       {
         title: "Authentication",
         icon: Icons.Authentication,
